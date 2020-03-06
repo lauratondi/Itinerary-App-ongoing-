c9d297f0e7d87ab59573842b938c9264060f93ae
Lauramars <lauramars85@gmail.com>
2020-03-06 16:50:27 +0100

    added proxi

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
--- keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI:
-        "mongodb+srv://lauramars85:MYtinerary@mytinerary-qtdlq.mongodb.net/myTinerary?retryWrites=true&w=majority",
-    jwtSecret: "my_secretToken"
-
-};
