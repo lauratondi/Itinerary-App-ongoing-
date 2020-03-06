fc623b4e915b27dd3239520dd21413ec305ba912
Lauramars <lauramars85@gmail.com>
2020-03-06 15:40:20 +0100

    showing keys in heroku

---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI:
+        "mongodb+srv://lauramars85:MYtinerary@mytinerary-qtdlq.mongodb.net/myTinerary?retryWrites=true&w=majority",
+    jwtSecret: "my_secretToken"
+
+};
